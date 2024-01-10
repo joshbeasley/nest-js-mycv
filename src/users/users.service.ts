0b83f0c669625cea0e68214295c016869ec8b636
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-10 15:22:23 -0800

    add remove service logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -36,5 +36,11 @@ export class UsersService {
     return this.repo.save(user)
   }
 
-  remove(){}
+  async remove(id: number){
+    const user = await this.repo.findOneBy({ id })
+    if (!user) {
+      throw new Error('user not found')
+    }
+    return this.repo.remove(user)
+  }
 }
