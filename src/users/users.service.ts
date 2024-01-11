b809f8447ba97b62624086532069d70e689000dc
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-11 11:34:06 -0800

    change errors to exceptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -29,7 +29,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>){
     const user = await this.repo.findOneBy({ id })
     if (!user) {
-      throw new Error('user not found')
+      throw new NotFoundException('user not found')
     }
 
     Object.assign(user, attrs)
@@ -39,7 +39,7 @@ export class UsersService {
   async remove(id: number){
     const user = await this.repo.findOneBy({ id })
     if (!user) {
-      throw new Error('user not found')
+      throw new NotFoundException('user not found')
     }
     return this.repo.remove(user)
   }
