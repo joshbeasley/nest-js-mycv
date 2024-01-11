d46939b644c092a6b5101dd32dea64ca10abf4eb
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-11 13:20:15 -0800

    finish sign-up controller logic

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -28,8 +28,10 @@ export class AuthService {
     const result = salt + '.' + hash.toString('hex')
 
     // create new user and save it
+    const user = await this.usersService.create(email, result)
 
     // return user
+    return user
   }
 
   signin() {
