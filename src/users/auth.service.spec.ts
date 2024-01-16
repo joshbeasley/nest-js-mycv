b79cbf56110daff23d2a4baac7a8996590160623
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-16 14:45:40 -0800

    refactor unit test to leverage beforeEach and describe blocks

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,25 +2,30 @@ import { Test } from "@nestjs/testing"
 import { AuthService } from "./auth.service"
 import { UsersService } from "./users.service"
 
+describe('AuthService', () => {
+  let service: AuthService
 
-it('can create an instance of auth service', async () => {
-  // create fake copy of users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({id: 1, email, password})
-  }
-  
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService
-      }
-    ]
-  }).compile()
+  beforeEach(async () => {
+    // create fake copy of users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) => Promise.resolve({id: 1, email, password})
+    }
+    
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        }
+      ]
+    }).compile()
 
-  const service = module.get(AuthService)
+    service = module.get(AuthService)
+  })
 
-  expect(service).toBeDefined()
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined()
+  })
 })
