6a080ce85c562a7a998e64d43787c910ad057df1
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-18 12:59:42 -0800

    add test for email in use

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -6,10 +6,11 @@ import { BadRequestException } from '@nestjs/common'
 
 describe('AuthService', () => {
   let service: AuthService
+  let fakeUsersService: Partial<UsersService>
 
   beforeEach(async () => {
     // create fake copy of users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
     }
@@ -40,12 +41,11 @@ describe('AuthService', () => {
     expect(hash).toBeDefined()
   })
 
-  // it('throws an error if user signs up with email that is in use', async () => {
-  //   fakeUsersService.find = () =>
- 
-  //     Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
-  //   await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
-  //     BadRequestException,
-  //   );
-  // });
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 })
