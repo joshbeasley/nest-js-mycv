b487bf56cc2981a4f4c0a3655c2c3f47003ff5c9
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-20 15:05:05 -0800

    add globally scoped pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
+import { Module, ValidationPipe } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { ReportsModule } from './reports/reports.module'
+import { User } from './users/user.entity'
+import { Report } from './reports/report.entity'
+import { APP_PIPE } from '@nestjs/core'
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true
-  }), UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true
+    }),
+    UsersModule,
+    ReportsModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ]
 })
 export class AppModule {}
