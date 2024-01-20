45d39dc431759de30c408994251dda7b4391fb9b
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-20 14:23:19 -0800

    refactor tests to make use of new fakeUsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -54,9 +54,7 @@ describe('AuthService', () => {
   })
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User])
-
+    await service.signup('asdf@asdf.com', 'asdf')
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     )
@@ -69,8 +67,7 @@ describe('AuthService', () => {
   })
 
   it('throws if an invalid password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ email: 'asdf@asdf.com', password: 'laskdjf' } as User])
+    await service.signup('laskdjf@alskdfj.com', 'password')
     await expect(
       service.signin('laskdjf@alskdfj.com', 'passowrd'),
     ).rejects.toThrow(BadRequestException)
