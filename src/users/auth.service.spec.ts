b98f59d21e5f70ab6b13375789d52463e07d60d4
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-20 13:59:47 -0800

    unused email test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,8 +1,8 @@
-import { Test } from "@nestjs/testing"
-import { AuthService } from "./auth.service"
-import { UsersService } from "./users.service"
-import { User } from "./user.entity"
-import { BadRequestException } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { AuthService } from './auth.service'
+import { UsersService } from './users.service'
+import { User } from './user.entity'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
 
 describe('AuthService', () => {
   let service: AuthService
@@ -12,17 +12,18 @@ describe('AuthService', () => {
     // create fake copy of users service
     fakeUsersService = {
       find: () => Promise.resolve([]),
-      create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
     }
-    
+
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
         {
           provide: UsersService,
-          useValue: fakeUsersService
-        }
-      ]
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile()
 
     service = module.get(AuthService)
@@ -42,10 +43,17 @@ describe('AuthService', () => {
   })
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
-    
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User])
+
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
-    );
-  });
+    )
+  })
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException)
+  })
 })
