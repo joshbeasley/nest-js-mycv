270877d94999f2426b9f23dc72f6760161e98e8a
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-20 14:30:36 -0800

    add method stubs for mock services in User controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,34 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
+import { AuthService } from './auth.service'
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let controller: UsersController
+  let fakeUsersService: Partial<UsersService>
+  let fakeAuthService: Partial<AuthService>
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: () => {},
+      find: () => {},
+      remove: () => {},
+      update: () => {},
+    }
+
+    fakeAuthService = {
+      signup: () => {},
+      signin: () => {},
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+    }).compile()
 
-    controller = module.get<UsersController>(UsersController);
-  });
+    controller = module.get<UsersController>(UsersController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
