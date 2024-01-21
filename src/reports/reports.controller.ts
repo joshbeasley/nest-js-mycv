1e01fd4cee67993dbef81944c797706d1fbe836f
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-20 20:11:43 -0800

    add get estimate endpoint

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards
+} from '@nestjs/common'
 import { CreateReportDto } from './dtos/create-report.dto'
 import { ReportsService } from './reports.service'
 import { AuthGuard } from 'src/guards/auth.guard'
@@ -8,6 +17,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor'
 import { ReportDto } from './dtos/report.dto'
 import { ApproveReportDto } from './dtos/approve-report.dto'
 import { AdminGuard } from 'src/guards/admin.guard'
+import { GetEstimateDto } from './dtos/get-estimate.dto'
 
 @Controller('reports')
 export class ReportsController {
@@ -24,5 +34,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved)
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query)
   }
 }
