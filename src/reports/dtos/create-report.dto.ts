94adecd8ba5dc662b8c812d5ac0dab12d65f3b45
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-20 18:44:25 -0800

    add validation on dto

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,15 +1,39 @@
-export class CreteReportDto {
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min
+} from 'class-validator'
+
+export class CreateReportDto {
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   price: number
 
+  @IsString()
   make: string
 
+  @IsString()
   model: string
 
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
   year: number
 
+  @IsNumber()
+  @IsLongitude()
   lng: number
 
+  @IsLatitude()
+  @IsNumber()
   lat: number
 
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   mileage: number
 }
